059580fd52fc0972e441828b3bdf2809f3262143
esme7383 <sylvain.mathieu@orange.com>
2020-05-18 13:13:40 +0200

    add typescript definition

--- binding.gyp
+++ binding.gyp
@@ -1,22 +1,21 @@
 {
-	"targets": [{
-		"target_name": "node_x11",
-			"sources": ["export.cc", "xdisplay.c"],
-			"include_dirs": [
-				"<!(node -e \"require('nan')\")"
-
-			],
-			"link_settings": {
-				"libraries": [
-					"-lXtst",
-				"-lX11",
-				"-lXfixes"
-				]
-
-			}
-
-	}
-
-	]
-
+  "targets": [
+    {
+      "target_name": "node_x11",
+      "sources": [
+        "src/cpp/export.cc",
+        "src/cpp/xdisplay.c"
+      ],
+      "include_dirs": [
+        "<!(node -e \"require('nan')\")"
+      ],
+      "link_settings": {
+        "libraries": [
+          "-lXtst",
+          "-lX11",
+          "-lXfixes"
+        ]
+      }
+    }
+  ]
 }
---
+++ src/js/@types/node-x11/index.d.ts
@@ -0,0 +1,33 @@
+declare module 'node-x11' {
+    enum MOUSE_BUTTON {
+        LEFT = 1,
+        MIDDLE = 2,
+        RIGHT= 3
+    }
+
+    export class XImage {
+        readonly width: number;
+        readonly height: number;
+        readonly depth: number;
+        readonly bits_per_pixel: number;
+        readonly bytes_per_line: number;
+        readonly data: Buffer;
+    }
+
+    export function init(display?: string);
+
+    export function getImageSync(withPointer?: boolean): XImage;
+
+    export function getImage(withPointer?: boolean, callback?: Function);
+
+    export function keyPressWithKeycode(keyCode: number, isDown: boolean);
+
+    export function keyPressWithKeysym(KeySym: number, isDown: boolean);
+
+    export function mouseMove(xPosition: number, yPosition: number);
+
+    export function mouseButton(button: number, isDown: boolean);
+
+    export function close();
+
+}
