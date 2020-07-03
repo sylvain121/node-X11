551122d8ebcf22f9d1fbc224982a7a6bd87f3282
esme7383 <sylvain.mathieu@orange.com>
2020-07-03 15:38:43 +0200

    add cursor declaration

--- src/js/@types/node-x11/index.d.ts
+++ src/js/@types/node-x11/index.d.ts
@@ -13,21 +13,27 @@ declare module 'node-x11' {
         readonly bytes_per_line: number;
         readonly data: Buffer;
     }
+
+    export class XCursor {
+        readonly x: number;
+        readonly y: number;
+        readonly xhot: number;
+        readonly yhot: number;
+        readonly cursorSerial: number;
+        readonly width: number;
+        readonly height: number;
+        readonly data: Buffer;
+    }
+
     type GetImageCallback = (err: Error, image: XImage) => void;
     export function init(display?: string): { width: number, height: number, depth: number };
-
     export function getImageSync(xoffset?: number, yoffset?: number, width?: number, height?: number): XImage;
-
     export function getImage(xoffset?:number, yoffset?: number, width?: number, height?: number, callback?: GetImageCallback);
-
     export function keyPressWithKeycode(keyCode: number, isDown: boolean);
-
     export function keyPressWithKeysym(KeySym: number, isDown: boolean);
-
     export function mouseMove(xPosition: number, yPosition: number);
-
     export function mouseButton(button: number, isDown: boolean);
-
+    export function getMouseCursorSync(): XCursor;
     export function close();
 
 }
